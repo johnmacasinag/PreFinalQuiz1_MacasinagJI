c387cb927bccc8a5ba05459a323fc338003c877a
johnmacasinag <johnian.macasinag@benilde.edu.ph>
2017-11-22 23:36:57 +0800

    Pre Final quiz 1 done

---
+++ PreFinalQuiz1_MacasinagJI/PreFinalQuiz1_MacasinagJI/App_Code/Tulong.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace PreFinalQuiz1_MacasinagJI.App_Code
+{
+    public class Tulong
+    {
+        public static string GetCon()
+        {
+            return ConfigurationManager.ConnectionStrings["HomeCon"].ConnectionString;
+        }
+    }
+}
---
+++ PreFinalQuiz1_MacasinagJI/PreFinalQuiz1_MacasinagJI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreFinalQuiz1_MacasinagJI.Startup))]
+namespace PreFinalQuiz1_MacasinagJI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
